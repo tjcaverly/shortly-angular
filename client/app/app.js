4543d3a18e09b33c1b9092d6c1ba56cd5fc2ec30
MKS-Student <mks.student@hackreactor.com>
2015-04-02 10:41:03 -0700

    Navigation between views and default route to links

--- client/app/app.js
+++ client/app/app.js
@@ -23,6 +23,11 @@ angular.module('shortly', [
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
     })
+    .otherwise({
+      templateUrl: 'app/links/links.html',
+      controller: 'LinksController'
+    });
+
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
